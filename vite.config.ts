d9bb521ba6f1e3fe8629ef0784fa56df371e2a17
Juliano Regis da Silva <157991737+julianoregisdasilva@users.noreply.github.com>
2026-07-05 19:52:58 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+ build: {
+    // Relative path string (relative to project root)
+    outDir: "dist",
+    // Optional: Forces Vite to empty the folder before building
+    emptyOutDir: true,
+  },
+
+  // Tells Vite the base public path when served in development or production, ensuring assets are correctly loaded on GitHub Pages.
+  base: "/",  
   plugins: [react(), tailwindcss(), tsconfigPaths()],
 });
